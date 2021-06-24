10bf0c4b2a0490b4e26eac7fa024bb211e93ae35
huotpanharithhs <72050103+huotpanharithhs@users.noreply.github.com>
2021-06-24 08:59:53 +0700

    first commit

---
+++ src/oop/TV.java
@@ -0,0 +1,12 @@
+package oop;
+
+public class TV {
+
+	public TV() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
